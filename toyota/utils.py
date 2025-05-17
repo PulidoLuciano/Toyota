dca5d2166439ae5b4844e6b86492a62fca1d8d16
Luciano Pulido <lucianonicolaspulido@gmail.com>
2025-05-17 13:24:47 -0300

    Make runs with kfold and log in mlflow

--- toyota/utils.py
+++ toyota/utils.py
@@ -1,4 +1,12 @@
 import pandas as pd
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import numpy as np
 
 def load_dataset(path: str) -> pd.DataFrame:
     return pd.read_csv(path, engine="python", encoding="utf8")
+
+def get_metrics(y_test: pd.Series, y_pred: pd.Series) -> dict:
+    mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    return {"mse": mse, "mae": mae, "rmse": rmse}

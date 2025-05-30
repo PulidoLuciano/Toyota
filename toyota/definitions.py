2a8f4dfd9b1ac1f13b939649301b9d0dbaa54e13
Luciano Pulido <lucianonicolaspulido@gmail.com>
2025-05-30 16:31:41 -0300

    Add pcr asset

--- toyota/definitions.py
+++ toyota/definitions.py
@@ -25,5 +25,9 @@ defs = Definitions(
             "mlflow_tracking_uri": "http://localhost:5000",
             "experiment_name": "toyota_lasso",
         }),
+        "mlflow_pca": mlflow_tracking.configured({
+            "mlflow_tracking_uri": "http://localhost:5000",
+            "experiment_name": "toyota_pca",
+        }),
     }
 )
